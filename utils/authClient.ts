86b3043b574d578074577c148acfd347429e18b4
darcy <darcy.ldx@gmail.com>
2025-04-11 13:28:35 -0400

    migrate to gitlab

--- utils/authClient.ts
+++ utils/authClient.ts
@@ -1,9 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://slqccoshyaaanseafwdv.supabase.co";
-const supabaseAnonKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNscWNjb3NoeWFhYW5zZWFmd2R2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzYxNDc4NTgsImV4cCI6MjA1MTcyMzg1OH0.YWjn2O6KivPMJGM5aSOOjaemWZ-ZI1mHaI4YYKZoJZg";
-
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || ""
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || ""
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
     autoRefreshToken: true,
